76c6dc75b8fe5af897cafb5077ade810f491e8ed
grev <grevano264@gmail.com>
2025-06-02 17:53:26 +0700

    Adding follows and likes tabel DB

---
+++ app/Http/Controllers/FollowsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\follows;
+use Illuminate\Http\Request;
+
+class FollowsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(follows $follows)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(follows $follows)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, follows $follows)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(follows $follows)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_06_02_105209_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->foreignId('userId')
+                  ->constrained('users', 'userId')
+                  ->onDelete('cascade'); 
+            $table->foreignId('postId')
+                  ->constrained('posts', 'postId') 
+                  ->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent(); 
+            $table->primary(['userId', 'postId']); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
