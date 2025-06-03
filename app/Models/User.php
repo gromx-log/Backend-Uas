4f1eb88b92e8f25dab3d2372218c9c2de9c96ffb
nallievira <nilleasier@gmail.com>
2025-06-03 11:57:12 +0700

    editprofile & view profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,13 +51,13 @@ class User extends Authenticatable
         ];
     }
 
-    public function followers()
-    {
-    return $this->belongsToMany(User::class, 'follows', 'following_id', 'follower_id');
-    }
+        public function followers()
+        {
+        return $this->belongsToMany(User::class, 'follows', 'following_id', 'follower_id');
+        }
 
-    public function following()
-    {
-    return $this->belongsToMany(User::class, 'follows', 'follower_id', 'following_id');
-    }
+        public function following()
+        {
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'following_id');
+        }
 }
